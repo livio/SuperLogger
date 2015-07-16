207c8bfb706dde4b7a3508a28e81219732374229
Joel Fischer <joel@livio.io>
2015-07-16 09:53:34 -0400

    Split out various loggers into separate optional frameworks

---
+++ SuperLogger-Console/SuperLogger-Console.h
@@ -0,0 +1,19 @@
+//
+//  SuperLogger-Console.h
+//  SuperLogger-Console
+//
+//  Created by Joel Fischer on 7/16/15.
+//  Copyright © 2015 livio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperLogger-Console.
+FOUNDATION_EXPORT double SuperLogger_ConsoleVersionNumber;
+
+//! Project version string for SuperLogger-Console.
+FOUNDATION_EXPORT const unsigned char SuperLogger_ConsoleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperLogger_Console/PublicHeader.h>
+
+
---
+++ SuperLogger-File/SuperLogger-File.h
@@ -0,0 +1,19 @@
+//
+//  SuperLogger-File.h
+//  SuperLogger-File
+//
+//  Created by Joel Fischer on 7/16/15.
+//  Copyright © 2015 livio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperLogger-File.
+FOUNDATION_EXPORT double SuperLogger_FileVersionNumber;
+
+//! Project version string for SuperLogger-File.
+FOUNDATION_EXPORT const unsigned char SuperLogger_FileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperLogger_File/PublicHeader.h>
+
+
