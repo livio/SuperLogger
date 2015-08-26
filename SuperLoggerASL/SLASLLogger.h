255ea2dfce73b826c0dd8c0b869e6a9caa8d4ecc
Joel Fischer <joeljfischer@gmail.com>
2015-08-26 10:45:55 -0400

    Add ASLLogger file

---
+++ SuperLoggerASL/SLASLLogger.h
@@ -0,0 +1,13 @@
+//
+//  SLASLLogger.h
+//  SuperLogger
+//
+//  Created by Joel Fischer on 8/26/15.
+//  Copyright © 2015 livio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLASLLogger : NSObject
+
+@end
---
+++ SuperLoggerASL/SLASLLogger.m
@@ -0,0 +1,13 @@
+//
+//  SLASLLogger.m
+//  SuperLogger
+//
+//  Created by Joel Fischer on 8/26/15.
+//  Copyright © 2015 livio. All rights reserved.
+//
+
+#import "SLASLLogger.h"
+
+@implementation SLASLLogger
+
+@end
