2c4e92f6bdfc6b42267e7a021c2f9b0814a18ffe
Joel Fischer <joeljfischer@gmail.com>
2015-08-27 14:11:13 -0400

    Make SLASLLogger.h public

--- SuperLoggerASL/SuperLoggerASL.h
+++ SuperLoggerASL/SuperLoggerASL.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char SuperLoggerASLVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SuperLoggerASL/PublicHeader.h>
 
-
+#import "SLASLLogger.h"
